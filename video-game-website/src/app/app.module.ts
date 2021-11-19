b29833501e27a2dd492630a4d3154587f12258e8
Sodinapalli-Karthik <sodinapallikarthik@gmail.com>
2021-11-20 01:22:44 +0530

    login and register form done commit on 'master'

--- video-game-website/src/app/app.module.ts
+++ video-game-website/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
 import { LoginpageComponent } from './components/loginpage/loginpage.component';
 import { RegisterformComponent } from './components/registerform/registerform.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent,
     LoginpageComponent,
     RegisterformComponent
   ],
