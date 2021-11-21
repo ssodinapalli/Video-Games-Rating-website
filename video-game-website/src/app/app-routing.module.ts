afa08a6e39f19195ad687be3367a952f850d480d
Sodinapalli-Karthik <sodinapallikarthik@gmail.com>
2021-11-21 12:00:53 +0530

    added routing to pages commit on 'master'

--- video-game-website/src/app/app-routing.module.ts
+++ video-game-website/src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './components/details/details.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import { RegisterformComponent } from './components/registerform/registerform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Login',
+    component: LoginpageComponent,
+  },
+  {
+    path: 'Register',
+    component: RegisterformComponent,
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+  },
+  {
+    path: '', redirectTo:'/register',pathMatch:'full'
+    
+  },
+  {
+    path: 'search/:game-search',
+    component: HomeComponent,
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- video-game-website/src/app/app.module.ts
+++ video-game-website/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginpageComponent } from './components/loginpage/loginpage.component';
 import { RegisterformComponent } from './components/registerform/registerform.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { GameTabsComponent } from './components/game-tabs/game-tabs.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginpageComponent,
-    RegisterformComponent
+    RegisterformComponent,
+    SearchbarComponent,
+    HomeComponent,
+    GameTabsComponent,
+    DetailsComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
